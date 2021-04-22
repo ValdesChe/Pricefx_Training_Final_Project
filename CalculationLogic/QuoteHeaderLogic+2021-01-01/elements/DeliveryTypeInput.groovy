4f717a7f22feca250ad6cc368ba17ffb6f0c2741
Valdes Che <vchezogou@cognitus.one>
2021-04-22 07:58:35 +0000

    freight surchage

--- CalculationLogic/QuoteHeaderLogic+2021-01-01/elements/DeliveryTypeInput.groovy
+++ CalculationLogic/QuoteHeaderLogic+2021-01-01/elements/DeliveryTypeInput.groovy
@@ -1,33 +1,3 @@
 if(quoteProcessor.isPrePhase()){
     return
 }
-
-List<Object> lineItems = quoteProcessor.quoteView.lineItems.findAll{ lineItem -> !lineItem.folder}
-
-BigDecimal sum = 0.0
-
-List<String> warnings = null
-for (lineItem in lineItems){
-    def itemInvoicePrice =  lineItem?.outputs?.find({output -> output.resultName == "TotalInvoicePrice"})?.result
-
-    if(itemInvoicePrice == null){
-        sum = null
-        warnings = ["Unable to calculate: value for TotalInvoicePrice is " +
-                                      "missing on one of the line items."]
-        break
-    }
-
-    sum += itemInvoicePrice ?: 0
-}
-
-// Add output
-def output = [
-        resultName   : "TotalInvoicePrice",
-        resultLabel  : "Total Invoice Price",
-        result       : sum,
-        formatType   : "MONEY_EUR",
-        resultType   : "SIMPLE",
-        cssProperties: "background-color:#99FFDD",
-        warnings     : warnings
-]
-quoteProcessor.addOrUpdateOutput(output)
