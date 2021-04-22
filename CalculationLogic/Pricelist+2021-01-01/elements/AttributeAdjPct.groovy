575a93e90793545e72c985c5f6b1a2e67928e919
Valdes Che <vchezogou@cognitus.one>
2021-04-22 09:41:07 +0000

    Split Pricelist calculation into functions in PriceListLib element

--- CalculationLogic/Pricelist+2021-01-01/elements/AttributeAdjPct.groovy
+++ CalculationLogic/Pricelist+2021-01-01/elements/AttributeAdjPct.groovy
@@ -1,6 +1,4 @@
-def productLifeCycle = api.product("ProductLifeCycle") // Get the product group of product
-def attributeAdjPct =  api.vLookup("AttributeAdj", "AttributeAdj", productLifeCycle)
-api.trace(productLifeCycle)
+def attributeAdjPct =  PriceListLib.attributeAdj()
 if(attributeAdjPct == null){
     api.addWarning("Unable to look up Attribute Adjustment with the Product Life Cycle")
     return
