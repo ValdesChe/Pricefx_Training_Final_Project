1a396f3431c4504c15fb5fc08e3c653cd04ff96f
Valdes Che <vchezogou@cognitus.one>
2021-04-23 10:02:52 +0000

    Created DimFilter using dropdow list

--- CalculationLogic/Dashboard_SalesManager+2021-03-23/elements/YearInputParameter.groovy
+++ CalculationLogic/Dashboard_SalesManager+2021-03-23/elements/YearInputParameter.groovy
@@ -1,1 +1,5 @@
-api.integerUserEntry("Year")
+def ctx = api.getDatamartContext()
+def dm = ctx.getDatamart("Transaction")
+def column = dm.getColumn("InvoiceDateYear")
+
+return ctx.dimFilterEntry("Year", column)?.getValue()
